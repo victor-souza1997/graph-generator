efffb1e923dbb4ef3eac62c77877ccec619e75b7
Joao Victor Lima de Souza <victor-souza1997@hotmail.com>
2024-08-04 07:25:35 -0400

    most compact way without touching in the bar position

--- stacked_columns_time.py
+++ stacked_columns_time.py
@@ -6,9 +6,9 @@ mp.rcParams.update({'font.size': 11}) # must set in top
 
 # Data to be plotted with time (ns) information
 data = [
-    ["Average\nThread Context\nSwitch\nUsing Yield (SLL)", 2215, 3321, 6638],
-    ["Average\nThread Context\nSwitch\nUsing Yield (RBT)", 3486, 5228, 10452],
-    ["Average\nThread Context\nSwitch\nUsing Yield (TQM)", 2269, 3402, 6801],
+    ["Average\nThread Context\nSwitch Using\nYield (SLL)", 2215, 3321, 6638],
+    ["Average\nThread Context\nSwitch Using\nYield (RBT)", 3486, 5228, 10452],
+    ["Average\nThread Context\nSwitch Using\nYield (TQM)", 2269, 3402, 6801],
     ["Average\nContext Switch\nTime Between\nThreads (SLL)", 2373, 3559, 7118],
     ["Average\nContext Switch\nTime Between\nThreads(RBT)", 5335, 8002, 16005],
     ["Average\nContext Switch\nTime Between\nThreads (TQM)", 2356, 3534, 7068]
@@ -23,12 +23,12 @@ df = pd.DataFrame(data, columns=["",
  
 # plot multiple columns such as population and year from dataframe
 df.plot(x="", y=["CPU 240 Mhz", "CPU 160 Mhz", "CPU 80 Mhz"],  yticks=np.arange(0, 17005, 2000),
-        kind="bar", figsize=(10, 10), rot =0,  color=["#77b41f", "#b41f2d", "#1fb4a7"],width=0.5)
+        kind="bar", figsize=(2.6, 2), rot =0,  color=["#77b41f", "#b41f2d", "#1fb4a7"],width=0.3)
 mp.ylabel("Time (ns)")
 mp.tight_layout()
 mp.ylim(0, 17005)
-ax = mp.gca()
-ax.set_box_aspect(1)
+#ax = mp.gca()
+#ax.set_box_aspect(1)
 
 
 
