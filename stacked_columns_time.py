4d3077bd17a6cfe88b8f026be87744a9e5211eb2
Joao Victor Lima de Souza <victor-souza1997@hotmail.com>
2024-08-03 21:06:36 -0400

    somethin

--- stacked_columns_time.py
+++ stacked_columns_time.py
@@ -1,29 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as mp
+import numpy as np
+
+mp.rcParams.update({'font.size': 11}) # must set in top
 
 # Data to be plotted with time (ns) information
 data = [
-    ["Average thread context\nswitch using yield\n(Group 1)", 2215, 3321, 6638],
-    ["Average thread context\nswitch using yield\n(Group 2)", 3486, 5228, 10452],
-    ["Average thread context\nswitch using yield\n(Group 3)", 2269, 3402, 6801],
-    ["Average context switch\ntime between threads\n(Group 1)", 2373, 3559, 7118],
-    ["Average context switch\ntime between threads\n(Group 2)", 5335, 8002, 16005],
-    ["Average context switch\ntime between threads\n(Group 3)", 2356, 3534, 7068]
+    ["Average\nThread Context\nSwitch\nUsing Yield (SLL)", 2215, 3321, 6638],
+    ["Average\nThread Context\nSwitch\nUsing Yield (RBT)", 3486, 5228, 10452],
+    ["Average\nThread Context\nSwitch\nUsing Yield (TQM)", 2269, 3402, 6801],
+    ["Average\nContext Switch\nTime Between\nThreads (SLL)", 2373, 3559, 7118],
+    ["Average\nContext Switch\nTime Between\nThreads(RBT)", 5335, 8002, 16005],
+    ["Average\nContext Switch\nTime Between\nThreads (TQM)", 2356, 3534, 7068]
 ]
 
-# Form DataFrame from data
-df = pd.DataFrame(data, columns=["Benchmark", 
-                                 "240 MHz", 
-                                 "160 MHz",
-                                 "80 MHz"])
-
-# Plot multiple columns such as context switch times from DataFrame
-df.plot(x="Benchmark", y=["240 MHz", "160 MHz", "80 MHz"],
-        kind="bar", figsize=(10, 2), rot=0)
-
-# Display plot
-mp.ylabel("Time (ns)")
+# form dataframe from data
+df = pd.DataFrame(data, columns=["", 
+                                 "CPU 240 Mhz", 
+                                 "CPU 160 Mhz",
+                                 "CPU 80 Mhz",
+                                ])
+ 
+# plot multiple columns such as population and year from dataframe
+df.plot(x="", y=["CPU 240 Mhz", "CPU 160 Mhz", "CPU 80 Mhz"],  yticks=np.arange(0, 17005, 2000),
+        kind="bar", figsize=(10, 10), rot =0,  color=["#77b41f", "#b41f2d", "#1fb4a7"],width=0.5)
 mp.tight_layout()
+mp.ylim(0, 17005)
+ax = mp.gca()
+ax.set_box_aspect(1)
 
-# Display plot
+
+
+# display plot
 mp.show()
