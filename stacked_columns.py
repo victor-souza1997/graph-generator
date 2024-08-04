67445585c483f16143d104e254767c1e63cf6068
Joao Victor Lima de Souza <victor-souza1997@hotmail.com>
2024-08-03 20:19:11 -0400

    final - at least

--- stacked_columns.py
+++ stacked_columns.py
@@ -2,31 +2,31 @@ import pandas as pd
 import matplotlib.pyplot as mp
 import numpy as np
 
-mp.rcParams.update({'font.size': 8}) # must set in top
+mp.rcParams.update({'font.size': 13}) # must set in top
 
 # data to be plotted
 
 
 data = [
-    ["Time to create\na thread\n(SLL)", 3160, 2203, 1252],
-    ["Time to create\na thread\n(RBT)", 3236, 2279, 1328],
-    ["Time to create\na thread\n(TQM)", 3160, 2203, 1252],
+    ["Time to\ncreate\n(SLL)", 3160, 2203, 1252],
+    ["Time to\ncreate\n(RBT)", 3236, 2279, 1328],
+    ["Time to\ncreate\n(TQM)", 3160, 2203, 1252],
 
-    ["Time to start\na thread\n(SLL)", 1499, 1187, 873],
-    ["Time to start\na thread\n(RBT)", 1730, 1417, 1103],
-    ["Time to start\na thread\n(TQM)", 1502, 1189, 876],
+    ["Time to\nstart\n(SLL)", 1499, 1187, 873],
+    ["Time to\nstart\n(RBT)", 1730, 1417, 1103],
+    ["Time to\nstart\n(TQM)", 1502, 1189, 876],
 
-    ["Time to suspend\na thread\n(SLL)", 724, 724, 724],
-    ["Time to suspend\na thread\n(RBT)", 914, 914, 914],
-    ["Time to suspend\na thread\n(TQM)", 761, 761, 761],
+    ["Time to\nsuspend\n(SLL)", 724, 724, 724],
+    ["Time to\nsuspend\n(RBT)", 914, 914, 914],
+    ["Time to\nsuspend\n(TQM)", 761, 761, 761],
 
-    ["Time to resume\na thread\n(SLL)", 681, 681, 681],
-    ["Time to resume\na thread\n(RBT)", 889, 889, 889],
-    ["Time to resume\na thread\n(TQM)", 724, 724, 724],
+    ["Time to\nresume\n(SLL)", 681, 681, 681],
+    ["Time to\nresume\n(RBT)", 889, 889, 889],
+    ["Time to\nresume\n(TQM)", 724, 724, 724],
     
-    ["Time to abort\na thread\n(SLL)", 709, 709, 395],
-    ["Time to abort\na thread\n(RBT)", 1021, 709, 1021],
-    ["Time to abort\na thread\n(TQM)", 1022, 710, 396]
+    ["Time to\nabort\n(SLL)", 709, 709, 395],
+    ["Time to\nabort\n(RBT)", 1021, 709, 1021],
+    ["Time to\nabort\n(TQM)", 1022, 710, 396]
 ]
 
 
@@ -39,8 +39,10 @@ df = pd.DataFrame(data, columns=["",
  
 # plot multiple columns such as population and year from dataframe
 df.plot(x="", y=["CPU 240 Mhz", "CPU 160 Mhz", "CPU 80 Mhz"],
-        kind="bar", figsize=(10, 1), rot =0, yticks=np.arange(0, 3750, 200))
+        kind="bar", figsize=(10, 1.8), rot =0, yticks=np.arange(0, 3750, 200),
+         color=["#77b41f", "#b41f2d", "#1fb4a7"])
 mp.ylabel("Clock Cycles")
 mp.tight_layout()
+mp.ylim(0, 3400)
 # display plot
 mp.show()
