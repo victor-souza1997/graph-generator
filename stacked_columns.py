da5d08c560a21c8de95362ab36f3e1202897bdef
Joao Victor Lima de Souza <victor-souza1997@hotmail.com>
2024-08-04 21:18:46 -0400

    something

--- stacked_columns.py
+++ stacked_columns.py
@@ -2,45 +2,35 @@ import pandas as pd
 import matplotlib.pyplot as mp
 import numpy as np
 
-mp.rcParams.update({'font.size': 15}) # must set in top
+mp.rcParams.update({'font.size': 15})  # must set in top
 
-# data to be plotted
-"""
-["Average Semaphore\nSignal Time (Group 1)", 135, 135, 135],
-        ["Average Semaphore\nSignal Time (Group 2)", 148, 148, 148],
-        ["Average Semaphore\nSignal Time (Group 3)", 135, 135, 135],
-        
-        ["Average Semaphore\nTest Time (Group 1)", 55, 55, 55],
-        ["Average Semaphore\nTest Time (Group 2)", 55, 55, 55],
-        ["Average Semaphore\nTest Time (Group 3)", 55, 55, 55],
-"""
-
+# Data to be plotted
 data = [
+    ["Semaphore\nTake Time\n(SLL)", 720, 720, 720],
+    ["Semaphore\nTake Time\n(RBT)", 948, 948, 948],
+    ["Semaphore\nTake Time\n(TQM)", 755, 755, 755],
+    ["Semaphore\nGive Time\n(SLL)", 1739, 1425, 1112],
+    ["Semaphore\nGive Time\n(RBT)", 2067, 1440, 1753],
+    ["Semaphore\nGive Time\n(TQM)", 1742, 1428, 1115]
+]
 
-        ["Semaphore\nTake Time\n(SLL)", 720, 720, 720],
-        ["Semaphore\nTake Time\n(RBT)", 948, 948, 948],
-        ["Semaphore\nTake Time\n(TQM)", 755, 755, 755],
-
-
-        ["Semaphore\nGive Time\n(SLL)", 1739, 1425, 1112],
-        ["Semaphore\nGive Time\n(RBT)", 2067, 1440, 1753],
-        ["Semaphore\nGive Time\n(TQM)", 1742, 1428, 1115]]
- 
-# form dataframe from data
+# Form dataframe from data
 df = pd.DataFrame(data, columns=["", 
                                  "CPU 240 Mhz", 
                                  "CPU 160 Mhz",
-                                 "CPU 80 Mhz",
-                                ])
- 
+                                 "CPU 80 Mhz"])
 
-# plot multiple columns such as population and year from dataframe
-df.plot(x="", y=["CPU 240 Mhz", "CPU 160 Mhz", "CPU 80 Mhz"],
-        kind="bar", figsize=(4, 4), rot =0, yticks=np.arange(0, 2100, 100),
-        color=["#77b41f", "#b41f2d", "#1fb4a7"],width=0.8)
+# Plot multiple columns such as population and year from dataframe
+ax = df.plot(x="", y=["CPU 240 Mhz", "CPU 160 Mhz", "CPU 80 Mhz"],
+             kind="bar", figsize=(8, 6), rot=0, yticks=np.arange(0, 2200, 100),
+             color=["#77b41f", "#b41f2d", "#1fb4a7"], width=0.8)
+
+# Annotate bars with their values
+for c in ax.containers:
+    ax.bar_label(c, label_type='edge', padding=3)
+
 mp.ylabel("Clock Cycles")
-
 mp.tight_layout()
-mp.ylim(0, 2100)
-# display plot
+mp.ylim(0, 2200)
+# Display plot
 mp.show()
