466ce4f77bab26e638735f0330cb4c5c1a841094
Joao Victor Lima de Souza <victor-souza1997@hotmail.com>
2024-08-04 20:24:10 -0400

    last commit

--- stacked_columns.py
+++ stacked_columns.py
@@ -4,24 +4,28 @@ import numpy as np
  
 mp.rcParams.update({'font.size': 13})
 # data to be plotted
-data = [["Switch from ISR \nBack to Interrupted \nThread (Group 1)", 1144, 830, 531],
-        ["Switch from ISR \nBack to Interrupted \nThread (Group 2)", 1144, 830, 531],
-        ["Switch from ISR \nBack to Interrupted \nThread (Group 3)", 1144, 830, 531],
-        ["Time from ISR to \nExecuting a Different \nThread (Group 1)", 451, 451, 451],
-        ["Time from ISR to \nExecuting a Different \nThread (Group 2)", 451, 451, 451],
-        ["Time from ISR to \nExecuting a Different \nThread (Group 3)", 451, 451, 451]]
+data = [["Switch from ISR\nBack to\nInterrupted\nThread (SLL)", 1144, 830, 531],
+        ["Switch from ISR\nBack to\nInterrupted\nThread (RBT)", 1144, 830, 531],
+        ["Switch from ISR\nBack to\nInterrupted\nThread (TQM)", 1144, 830, 531],
+        ["Time from ISR\nto Executing a\nDifferent\nThread (SLL)", 451, 451, 451],
+        ["Time from ISR\nto Executing a\nDifferent\nThread (RBT)", 451, 451, 451],
+        ["Time from ISR\nto Executing a\nDifferent\nThread (TQM)", 451, 451, 451]]
  
 # form dataframe from data
-df = pd.DataFrame(data, columns=["Benchmarks Divided by Group", 
+df = pd.DataFrame(data, columns=["", 
                                  "CPU 240 Mhz", 
                                  "CPU 160 Mhz",
                                  "CPU 80 Mhz",
                                 ])
  
+
 # plot multiple columns such as population and year from dataframe
-df.plot(x="Benchmarks Divided by Group", y=["CPU 240 Mhz", "CPU 160 Mhz", "CPU 80 Mhz"],
-        kind="bar", figsize=(10, 10), rot =0, yticks=np.arange(0, 1200, 50))
+df.plot(x="", y=["CPU 240 Mhz", "CPU 160 Mhz", "CPU 80 Mhz"],
+        kind="bar", figsize=(6, 6), rot =0, yticks=np.arange(0, 1200, 100),
+        color=["#77b41f", "#b41f2d", "#1fb4a7"],width=0.8)
 mp.ylabel("Clock Cycles")
 
+mp.tight_layout()
+mp.ylim(0, 1200)
 # display plot
 mp.show()
